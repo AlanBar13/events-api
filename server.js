e186f57d273a13d204af2125532a0a94422e6ffa
Alan Bardales <alan.g.bardales@gmail.com>
2021-06-15 13:18:39 -0500

    Added mongo dependencies and connect local DB

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import connectDB from './api/config/database.js'
 
 const app = express()
+dotenv.config()
 
 app.use(express.json())
 app.use(cors())
+connectDB()
 
 app.use('/', (req, res) => {
     res.json({"message": "Visistant registration API..."})
