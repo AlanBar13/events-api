14d18cefecc89c052f98c8a0b126591836adf0fd
Alan Bardales <alan.g.bardales@gmail.com>
2021-06-16 10:57:04 -0500

    Added error middleware

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './api/config/database.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './api/config/database.js';
 
-import userRoutes from './api/routes/users.routes.js'
+import userRoutes from './api/routes/users.routes.js';
+
+import { notFound, errorHandler } from './api/middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send({"message": "Visistant registration API..."})
+    res.send({"message": "Visistant registration API..."});
 });
 
 //ROUTES
-app.use("/api/users", userRoutes)
+app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 4000
+//MIDDLEWARE
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, 
-    console.log(`Server running as ${process.env.MODE} in ${PORT}`))
+    console.log(`Server running as ${process.env.MODE} in ${PORT}`));
